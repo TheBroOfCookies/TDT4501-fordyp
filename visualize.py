3824d9b74689ec7a7387ba43aa61bc2a92db718e
TheBroOfCookies <345hot345@hotmail.no>
2024-10-01 10:42:14 +0200

    Updated and expanded visualizer

--- visualize.py
+++ visualize.py
@@ -1,7 +1,7 @@
 from time import gmtime, strftime
 import matplotlib.pyplot as plt
 
-def make_plot(filename):
+def get_first_vx(filename):
     points = []
     with open(filename) as file:
         lines = file.readlines()
@@ -12,19 +12,75 @@ def make_plot(filename):
             points.append(float(line))
     return points
 
+def get_nth_vx(filename, n):
+    points = []
+    with open(filename) as file:
+        lines = file.readlines()
+        count = 0
+        Vx = False
+        for i in range(2,len(lines)):
+            line = lines[i]
+            if line == "Vz\n" or line == "Vy\n" or line == "\n":
+                Vx = False
+            elif line == "Vx\n":
+                Vx = True
+                count += 1
+            elif Vx and count == n:
+                points.append(float(line))
+    return points
+
+def get_nth_vy(filename, n):
+    points = []
+    with open(filename) as file:
+        lines = file.readlines()
+        count = 0
+        Vy = False
+        for i in range(2,len(lines)):
+            line = lines[i]
+            if line == "Vx\n" or line == "Vz\n" or line == "\n":
+                Vy = False
+            elif line == "Vy\n":
+                Vy = True
+                count += 1
+            elif Vy and count == n:
+                points.append(float(line))
+    return points
+
+def get_nth_vz(filename, n):
+    points = []
+    with open(filename) as file:
+        lines = file.readlines()
+        count = 0
+        Vz = False
+        for i in range(2,len(lines)):
+            line = lines[i]
+            if line == "Vx\n" or line == "Vy\n" or line == "\n":
+                Vz = False
+            elif line == "Vz\n":
+                Vz = True
+                count += 1
+            elif Vz and count == n:
+                points.append(float(line))
+    return points
 
 
-    
-    
-
-points1 = make_plot("receivers.csv")
-points2 = make_plot("receivers_mpi.csv")
 title = "Sequential+parallel__expanded_kernel,HALO=4"
+#title = "Parallel__expanded_kernel,HALO=4"
+#title = "Sequential__expanded_kernel,HALO=4"
 plt.title(title)
-plt.plot(points1, color= "b")
-plt.plot(points2, color= "r")
-plt.legend(["Sequential", "Parallel, n=2"])
+for i in range(1,9):
+    plt.subplot(2,4,i)
+    points1 = get_nth_vx("receivers.csv", i)
+    points2 = get_nth_vx("receivers_mpi.csv", i)
+    sub_title = "Vx="+str(i)
+    plt.title(sub_title)
+    plt.plot(points1, color= "b")
+    plt.plot(points2, color= "r")
+#plt.subplot(2,4,1)
+#plt.legend(["Sequential", "Parallel, n=2"])
+
 time = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+title = title + " all Vx"
 #plt.savefig("figures/"+title+"__"+time)
 plt.show()
             
